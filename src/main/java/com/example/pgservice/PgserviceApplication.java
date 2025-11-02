005200651800f60bca756a357a62a029fa3b61db
harshadal2506 <hsladole@gmail.com>
2025-11-02 11:08:05 +0530

    Add files via upload

---
+++ src/main/java/com/example/pgservice/PgserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pgservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pgservice/PgserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pgservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
